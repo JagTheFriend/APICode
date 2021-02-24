a4453c2d44fad1cb37aff134042bb72dd1245b68
JagTheFriend <jagthefriend12@gmail.com>
2021-02-24 17:10:34 +0200

    Making a new lyrics api

---
+++ lyrics_api.py
@@ -0,0 +1,20 @@
+import os
+from lyrics_extractor import SongLyrics
+
+def song_lyrics(song):
+    """Gets the lyrics of a song
+
+    Arguments:
+    @song => Name of the song
+
+    :return: Python dictionary"""
+    
+    if song:
+        extract_lyrics = SongLyrics(os.environ.get(
+            "lyrics_token"), os.environ.get("GCS_ENGINE_ID")
+        )
+
+        lyrics = extract_lyrics.get_lyrics(" ".join(song)).get("lyrics", "No lyrics was found")
+        return {"output": lyrics}
+
+    return {"output": "Also please send the name of the song"}
