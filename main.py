5a6dba3f0d2e2aed9f1833edc53bd7b87c7a3188
JagTheFriend <jagthefriend12@gmail.com>
2021-02-24 17:10:45 +0200

    Added it to main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask
 # project imports
 import compiler_api
 import meme_api
+import lyrics_api
 
 app = Flask(__name__)
 
@@ -40,6 +41,18 @@ def compile(lang, code):
 
     :return: Python dictionary"""
     return compiler_api._compile(lang=lang, code=code)
+
+
+@app.route('/lyrics=<song>')
+def lyrics(song):
+    """Gets the lyrics of a song
+
+    Arguments:
+    @lyrics => Name of the lyrics
+
+    :return: Python dictionary"""
+
+    return lyrics_api.song_lyrics(song)
 
 
 if __name__ == "__main__":
