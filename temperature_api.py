66170e0880fdeed5d09ca39856a8fab7bf30bdb9
JagTheFriend <jagthefriend12@gmail.com>
2021-03-07 16:27:23 +0200

    Added tempreture api, which gets the tempreture of a place

---
+++ temperature_api.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+appid = os.environ.get('weather')
+
+def temp(*, city):
+    """
+    It finds out the temperature of a city
+
+    Arguments:
+        @city => Name of the city
+
+    :return: Python Dictionary
+    """
+
+    result = {}
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
+    try:
+        data = requests.get(url).json()
+        result["temp"] = data['main']['temp']
+        result["wind_speed"] = data['wind']['speed']
+
+        result["latitude"] = data['coord']['lat']
+        result["longitude"] = data['coord']['lon']
+
+        result["descriptions"] = data['weather'][0]['description']
+
+    except Exception:
+        result["output"] = "Invalid City name | Or the API Does not understand"
+
+    return result
