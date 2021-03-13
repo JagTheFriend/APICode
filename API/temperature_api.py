4823b64d30b409f2b5df942abad5d039dd4fbba6
JagTheFriend <jagthefriend12@gmail.com>
2021-03-13 18:39:51 +0200

    Fixed bugs

--- API/temperature_api.py
+++ API/temperature_api.py
@@ -15,13 +15,14 @@ def temp(*, city) -> dict:
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
     try:
         data = requests.get(url).json()
-        result["temp"] = data['main']['temp']
-        result["wind_speed"] = data['wind']['speed']
+        result["output"] = {}
+        result["output"]["temp"] = data['main']['temp']
+        result["output"]["wind_speed"] = data['wind']['speed']
 
-        result["latitude"] = data['coord']['lat']
-        result["longitude"] = data['coord']['lon']
+        result["output"]["latitude"] = data['coord']['lat']
+        result["output"]["longitude"] = data['coord']['lon']
 
-        result["descriptions"] = data['weather'][0]['description']
+        result["output"]["descriptions"] = data['weather'][0]['description']
 
     except Exception:
         result["output"] = "Invalid City name | Or the API Does not understand"
