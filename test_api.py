5254599ac3ed2a448d891811678f0a4752ee0957
JagTheFriend <jagthefriend12@gmail.com>
2021-03-22 15:10:16 +0200

    Added license to all the files

--- test_api.py
+++ test_api.py
@@ -1,3 +1,25 @@
+################################################################################
+# Copyright (c) 2021 JagTheFriend
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+################################################################################
+
 # NECESSARY IMPORTS
 from unittest import TestCase
 import requests
@@ -43,7 +65,7 @@ Main_page = "Providing service to other user(s) <br> Here is my code: <a href='h
 class CalculationTests(TestCase):
     def test_calculator(self):
         """
-        Checks: 
+        Checks:
             whether the result of the calculator is correct or not
         """
 
@@ -56,7 +78,7 @@ class CalculationTests(TestCase):
     def test_hex_to_denary(self):
         """
         Checks:
-            whether the result of hexadecimal to denary is correct or not        
+            whether the result of hexadecimal to denary is correct or not
         """
 
         result = requests.get(
